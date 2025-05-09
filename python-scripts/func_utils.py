1f672f1f10a68683ea5dcdcbc3fe5ae3e8adf0ec
Sorahawk <sorahawktech@gmail.com>
2025-05-09 14:23:23 +0800

    send full traceback to Discord as a text file instead of a direct string so it won't hit the 4000 character limit

--- python-scripts/func_utils.py
+++ python-scripts/func_utils.py
@@ -48,4 +48,4 @@ async def make_get_request(url, use_proxy=False, get_content=False):
 # obtains full traceback of given exception and outputs to specified channel
 async def send_traceback(e, channel):
 	full_trace = ''.join(traceback.format_exception(type(e), e, e.__traceback__))
-	await channel.send(f'```{full_trace}```')
+	await channel.send(file=discord.File(io.StringIO(full_trace), filename="traceback.txt"))
