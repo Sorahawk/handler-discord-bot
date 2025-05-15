d4bf04030805a95c2d2d2fe3a9f9e5879ee910e2
Sorahawk <sorahawktech@gmail.com>
2025-05-15 17:00:56 +0800

    Swapped feed order again

--- python-scripts/bot_methods.py
+++ python-scripts/bot_methods.py
@@ -20,8 +20,8 @@ async def quest_method(message, user_input, flag_presence):
 async def status_method(message, user_input, flag_presence):
 	status_list = [
 		"WILDS_NOTICE_LIST",
-		"WILDS_SUPPORT_LIST",
 		"WILDS_UPDATE_LIST",
+		"WILDS_SUPPORT_LIST",
 		"WILDS_NEWS_LIST",
 		"MH_NEWS_LIST",
 	]
