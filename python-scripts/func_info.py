7192d75602277d1ad66594061eeb4951fb7b73c9
Sorahawk <sorahawktech@gmail.com>
2025-10-11 11:00:02 +0800

    update

--- python-scripts/func_info.py
+++ python-scripts/func_info.py
@@ -40,11 +40,8 @@ def check_wilds_news(html_data):
 	item_list = html_data.find_class('news-container')[0].find_class('news-item')
 	for item in item_list:
 		# format date
-		date = item.find_class('news-item__ymd')[0].text_content()  # set embed date to release date if it has already passed, otherwise current date
-		
-		await var_global.INFO_CHANNEL.send(f"data: {date}")
-		
-		dt = datetime.strptime(date, '%Y.%m.%d') if date else datetime.now()
+		date = item.find_class('news-item__ymd')[0].text_content()
+		dt = datetime.strptime(date.strip(), '%Y.%m.%d') if date else datetime.now()
 
 		# construct identifier string
 		formatted_date = format_identifier_date(dt)
